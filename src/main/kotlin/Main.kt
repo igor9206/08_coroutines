100f52af19ad15f9f89b1782ca269933cfa5e98e
Korshunov Igor <igor.9206@gmail.com>
2023-10-23 19:26:45 +0600

    git init

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,90 @@
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.coroutines.*
+import okhttp3.*
+import okhttp3.logging.HttpLoggingInterceptor
+import java.io.IOException
+import java.util.concurrent.TimeUnit
+import kotlin.coroutines.EmptyCoroutineContext
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
+private val gson = Gson()
+private val BASE_URL = "http://127.0.0.1:9999"
+private val client = OkHttpClient.Builder()
+    .addInterceptor(HttpLoggingInterceptor(::println).apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    })
+    .connectTimeout(30, TimeUnit.SECONDS)
+    .build()
+
+fun main() {
+    with(CoroutineScope(EmptyCoroutineContext)) {
+        launch {
+            try {
+                val posts = getPosts(client)
+                    .map { post ->
+                        async {
+                            FullPost(
+                                post,
+                                getAuthor(post.authorId),
+                                getComment(post.id).map {
+                                    CommentsPost(it, getAuthor(it.authorId))
+                                }
+                            )
+                        }
+                    }.awaitAll()
+                posts.forEach {
+                    println(it)
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    Thread.sleep(30_000L)
+}
+
+suspend fun OkHttpClient.apiCall(url: String): Response {
+    return suspendCoroutine { continuation ->
+        Request.Builder()
+            .url(url)
+            .build()
+            .let(::newCall)
+            .enqueue(object : Callback {
+                override fun onResponse(call: Call, response: Response) {
+                    continuation.resume(response)
+                }
+
+                override fun onFailure(call: Call, e: IOException) {
+                    continuation.resumeWithException(e)
+                }
+            })
+    }
+}
+
+suspend fun <T> makeRequest(url: String, client: OkHttpClient, typeToken: TypeToken<T>): T =
+    withContext(Dispatchers.IO) {
+        client.apiCall(url)
+            .let { response ->
+                if (!response.isSuccessful) {
+                    response.close()
+                    throw RuntimeException(response.message)
+                }
+                val body = response.body ?: throw RuntimeException("response body is null")
+                gson.fromJson(body.string(), typeToken.type)
+            }
+    }
+
+suspend fun getPosts(client: OkHttpClient): List<Post> =
+    makeRequest("$BASE_URL/api/posts", client, object : TypeToken<List<Post>>() {})
+
+suspend fun getAuthor(authorId: Long): Author {
+    return makeRequest("$BASE_URL/api/authors/$authorId", client, object : TypeToken<Author>() {})
+}
+
+suspend fun getComment(id: Long): List<Comment> {
+    return makeRequest("$BASE_URL/api/posts/$id/comments", client, object : TypeToken<List<Comment>>() {})
+}
